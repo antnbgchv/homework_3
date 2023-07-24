9aaf0cf2eba9ccb3b69c76aa2bc44deee36e3e01
bogachev <antn.bgchv@gmail.com>
2023-07-24 13:48:04 +0300

    added empty yandex test

---
+++ src/test/java/YandexTest.java
@@ -0,0 +1,20 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class YandexTest {
+
+    @BeforeAll
+    static void config() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void simpleYandexSearchTest() {
+
+    }
+}
