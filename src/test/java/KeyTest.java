b923f1455365663953d613f1f7e1cf7775504835
bogachev <antn.bgchv@gmail.com>
2023-07-24 13:27:22 +0300

    added google test

---
+++ src/test/java/KeyTest.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class KeyTest {
+
+    @BeforeAll
+    static void config() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void SimpleSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("youtube").pressEnter();
+        $("[id=search]").shouldHave(text("youtube.com"));
+    }
+}
